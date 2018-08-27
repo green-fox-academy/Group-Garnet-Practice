e58892e1dafd48728ec03d649e3120a3c5d4b4be
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-27 11:12:02 +0800

    c6

--- count.js
+++ count.js
@@ -1,5 +1,7 @@
-function count(age) {
-    console.log(age);
+function count(parameter) {
+    console.log(`Counting the countless ${parameter}`);
 }
 
 const age = '1 2 3fdasfsdfasfdasfasdf masdfasfadsdfsfasfsdfdasfsfasasfu tou ren!!!';
+
+count(age);
