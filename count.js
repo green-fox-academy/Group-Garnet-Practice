14ddb2be8bd2cc775aefaa6fab9bab19f56db8ad
Jimmy_Yao <Jimmy_Yao@epam.com>
2018-08-27 10:44:59 +0800

    b5

--- count.js
+++ count.js
@@ -1,5 +1,7 @@
-function count(age) {
-    console.log(age);
+function count(age1, age2) {
+    console.log(age1);
+    console.log(age2);
 }
 
 const age = '1 2 3fdasfsdfasfdasfasdf masdfasfadsdfsfasfsdfdasfsfasasfu tou ren!!!';
+count(age,'123');
